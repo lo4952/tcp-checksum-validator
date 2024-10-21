8a7f3471df0247256699e05943d1e34d0bf04bae
logan <lo4952@vt.edu>
2024-10-21 14:06:31 -0400

    Initial commit, hello world

---
+++ tcp_checksum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Open a socket and capture a tcp packet
+
+
+
+// Isolate the provided tcp header checksum
+
+
+
+// Manually calculate tcp header checksum
+
+
+
+// Compare the calculated and given checksums
+
+
+
+int main(int argc, const char* argv) {
+    printf("Hello world\n");
+    return 1;
+}
